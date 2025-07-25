d17ea559eaa425efe58b61eb1a7411a3485db23a
ek <ek@rocketsoftware.com>
2025-07-25 11:49:35 +0530

    test with 3.2.5 to upload to test pypi

--- ibm_db.c
+++ ibm_db.c
@@ -22,7 +22,7 @@
 +--------------------------------------------------------------------------+
 */
 
-#define MODULE_RELEASE "3.2.6"
+#define MODULE_RELEASE "3.2.5"
 
 #include <Python.h>
 #include <datetime.h>
