c2fbfe049751eb1b8ada3cbb20bfe97d4bd9a62a
Earamma K <ek@rocketsoftware.com>
2026-04-06 10:56:06 +0530

    Trigger testpypi publish 3.2.6.5

--- ibm_db.c
+++ ibm_db.c
@@ -22,7 +22,7 @@
 +--------------------------------------------------------------------------+
 */
 
-#define MODULE_RELEASE "3.2.6.4"
+#define MODULE_RELEASE "3.2.6.5"
 
 #include <Python.h>
 #include <datetime.h>
