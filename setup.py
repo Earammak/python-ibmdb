303024a1b392c944dbfcdd522287c7c434d09b31
Earamma K <ek@rocketsoftware.com>
2026-09-11 10:25:19 +0530

    Add Python 3.15 wheel support and resolve SPARRAY tests on zLinux/s390x
    Signed-off-by: Earamma K <ek@rocketsoftware.com>

--- setup.py
+++ setup.py
@@ -560,6 +560,7 @@ setup( name    = PACKAGE,
                     'Programming Language :: Python :: 3.12',
                     'Programming Language :: Python :: 3.13',
                     'Programming Language :: Python :: 3.14',
+                    'Programming Language :: Python :: 3.15',
                     'Topic :: Database :: Front-Ends'],
 
     long_description = open(readme).read(),
